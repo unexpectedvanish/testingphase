db48e40c94f6843388efd870e683b1d03e3de27d
unexpectedvanish <alonelasw@gmail.com>
2025-12-09 13:09:34 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -9,17 +9,44 @@ local p = Players.LocalPlayer
 local mouse = p:GetMouse()
 local pGui = p:WaitForChild("PlayerGui")
 
---{}[]
-
-local variables = {
-    CtoTeleport = false,
-    ESP = false,
-    Aimbot = false,
-    Spintbot = false
+local limbs = {
+    "Head",
+    "Torso",
+    "Left Arm",
+    "Right Arm",
+    "Left Leg",
+    "Right Leg",
+    "HumanoidRootPart",
+    "UpperTorso",
+    "LowerTorso",
+    "LeftUpperArm",
+    "LeftLowerArm",
+    "LeftHand",
+    "RightUpperArm",
+    "RightLowerArm",
+    "RightHand",
+    "LeftUpperLeg",
+    "LeftLowerLeg",
+    "LeftFoot",
+    "RightUpperLeg",
+    "RightLowerLeg",
+    "RightFoot"
 }
 
+local espConn
+local namesConn
+local speedConn
+local jumpConn
+local spinbotConn
+local glueConn
+
+--{}[]
+
 mouse.Button1Down:Connect(function()
-    if (variables.CtoTeleport == false) then return end
+    if (not pGui:FindFirstChild("CCoreGui")) then return end
+    local gui = pGui:FindFirstChild("CCoreGui")
+    if (not gui:FindFirstChild("CtoTeleport")) then return end
+    if (gui:FindFirstChild("CtoTeleport").Value == false) then return end
     local hitPos = mouse.Hit
     local char = p.Character
     if (char) and (hitPos) and (char:FindFirstChild("HumanoidRootPart")) then
@@ -48,7 +75,179 @@ local function TeleportPlayer(givenName)
     end
 end
 
-local function CreateButton(name, parent)
+local function ToggleEsp(bool)
+    if (bool == true) then
+        espConn = runService.RenderStepped:Connect(function()
+            for i, plrs in pairs(Players:GetPlayers()) do
+                if (plrs) and (plrs.Character) then
+                    local char = plrs.Character
+                    if (char) then
+
+                        if (plrs == p) then continue end
+
+                        if (plrs.Character:FindFirstChild("ESPLight")) then
+                            if (pGui.CCoreGui.Chams.Value == false) then
+                                plrs.Character:FindFirstChild("ESPLight").FillTransparency = 1
+                            else
+                                plrs.Character:FindFirstChild("ESPLight").FillTransparency = .5
+                            end
+                            continue
+                        end
+
+                        local highlight = Instance.new("Highlight", plrs.Character)
+                        highlight.Name = "ESPLight"
+
+                        for i, v in pairs(plrs.Character:GetChildren()) do
+                            if (limbs[v.Name]) then
+                                highlight.Parent = v.Parent
+                            end
+                        end
+
+                        if (pGui.CCoreGui.Chams.Value == false) then
+                            plrs.Character:FindFirstChild("ESPLight").FillTransparency = 1
+                        else
+                            plrs.Character:FindFirstChild("ESPLight").FillTransparency = .5
+                        end
+
+                    end
+                end
+            end
+        end)
+    else
+        if (espConn) then
+            espConn:Disconnect()
+        end
+        for i, plrs in pairs(Players:GetPlayers()) do
+            if (plrs) and (plrs.Character) then
+                if (plrs.Character:FindFirstChild("ESPLight")) then
+                    plrs.Character:FindFirstChild("ESPLight"):Destroy()
+                end
+            end
+        end
+    end
+end
+
+local function ToggleNames(bool)
+    if (bool == true) then
+        namesConn = runService.RenderStepped:Connect(function()
+            for i, plrs in pairs(Players:GetPlayers()) do
+                if (plrs) and (plrs.Character) then
+                    if (plrs.Character:FindFirstChild("Head")) then
+                        if (plrs.Character.Head:FindFirstChild("BiggerName")) then continue end
+
+                        local billboard = Instance.new("BillboardGui", plrs.Character.Head)
+                        billboard.Name = "BiggerName"
+                        billboard.StudsOffset = Vector3.new(0,3.5,0)
+                        billboard.Size = UDim2.fromScale(9,1.5)
+                        billboard.MaxDistance = 250
+
+                        local textlabel = Instance.new("TextLabel", billboard)
+                        textlabel.BackgroundTransparency = 1
+                        textlabel.Size = UDim2.fromScale(1,1)
+                        textlabel.Font = Enum.Font.ArimoBold
+                        textlabel.TextColor3 = Color3.fromRGB(255,255,255)
+                        textlabel.TextScaled = true
+                        textlabel.Text = plrs.DisplayName.."(@"..plrs.Name..")"
+
+                        local stroke = Instance.new("UIStroke", textlabel)
+                    end
+                end
+            end
+        end)
+    else
+        if (namesConn) then
+            namesConn:Disconnect()
+        end
+        for i, plrs in pairs(Players:GetPlayers()) do
+            if (plrs) and (plrs.Character) and (plrs.Character:FindFirstChild("Head")) then
+                if (plrs.Character.Head:FindFirstChild("BiggerName")) then
+                    plrs.Character.Head.BiggerName:Destroy()
+                end
+            end
+        end
+    end
+end
+
+local function ToggleSpeed(bool, value)
+    if (bool == true) then
+        speedConn = runService.RenderStepped:Connect(function()
+            if (p.Character) and (p.Character:FindFirstChildWhichIsA("Humanoid")) then
+                p.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = value
+            end
+        end)
+    else
+        if (speedConn) then
+            speedConn:Disconnect()
+        end
+        if (p.Character) and (p.Character:FindFirstChildWhichIsA("Humanoid")) then
+            p.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = 16
+        end
+    end
+end
+
+local function ToggleJump(bool, value)
+    if (bool == true) then
+        jumpConn = runService.RenderStepped:Connect(function()
+            if (p.Character) and (p.Character:FindFirstChildWhichIsA("Humanoid")) then
+                p.Character:FindFirstChildWhichIsA("Humanoid").UseJumpPower = false
+                p.Character:FindFirstChildWhichIsA("Humanoid").JumpHeight = value
+            end
+        end)
+    else
+        if (jumpConn) then
+            jumpConn:Disconnect()
+        end
+        if (p.Character) and (p.Character:FindFirstChildWhichIsA("Humanoid")) then
+            p.Character:FindFirstChildWhichIsA("Humanoid").JumpHeight = 7.2
+        end
+    end
+end
+
+local function ToggleSpinbot(bool)
+    if (bool == true) then
+        spinbotConn = runService.RenderStepped:Connect(function()
+            local hrp = p.Character:FindFirstChild("HumanoidRootPart")
+            if (hrp) then
+                local random = math.random(0,120)
+                hrp.CFrame = hrp.CFrame * CFrame.Angles(0, math.rad(random), 0)
+            else
+                if (spinbotConn) then
+                    spinbotConn:Disconnect()
+                end
+            end
+        end)
+    else
+        if (spinbotConn) then
+            spinbotConn:Disconnect()
+        end
+    end
+end
+
+local function ToggleGlueHack(bool, user)
+    if (bool == true) then
+        for i, plrs in pairs(Players:GetPlayers()) do
+            if (plrs.Name == user) or (plrs.DisplayName == user) then
+                glueConn = runService.RenderStepped:Connect(function()
+                    if (p.Character) and (plrs.Character) then
+                        if (p.Character:FindFirstChild("HumanoidRootPart")) and (plrs.Character:FindFirstChild("HumanoidRootPart")) then
+                            p.Character.HumanoidRootPart.CFrame = plrs.Character.HumanoidRootPart.CFrame * CFrame.new(
+                                math.random(.2,.5),
+                                math.random(.2,.5),
+                                math.random(.2,.5)
+                            )
+                        end
+                    end
+                end)
+            end
+        end
+    else
+        if (glueConn) then
+            glueConn:Disconnect()
+        end
+    end
+end
+
+local function CreateButton(name, parent, gui)
     local barFrame = Instance.new("CanvasGroup", parent)
     barFrame.BackgroundColor3 = Color3.fromRGB(61,61,61)
     barFrame.Size = UDim2.new(0.197, 0,0.64, 0)
@@ -79,13 +278,13 @@ local function CreateButton(name, parent)
     })
 
     button.Activated:Connect(function()
-        local status = variables[button.Name]
-        if (status == false) then
+        local status = gui:FindFirstChild(name)
+        if (status) and (gui:FindFirstChild(name).Value == false) then
             onAnim:Play()
-            variables[button.Name] = true
+            gui:FindFirstChild(button.Name).Value = true
         else
             offAnim:Play()
-            variables[button.Name] = false
+            gui:FindFirstChild(button.Name).Value = false
         end
     end)
 
@@ -93,11 +292,40 @@ local function CreateButton(name, parent)
     buttonCorner.CornerRadius = UDim.new(1,0)
 end
 
+local function SetupBools(parent)
+    local spinbot = Instance.new("BoolValue", parent)
+    spinbot.Name = "Spinbot"
+    spinbot.Changed:Connect(function()
+        ToggleSpinbot(spinbot.Value)
+    end)
+    local names = Instance.new("BoolValue", parent)
+    names.Name = "Names"
+    names.Changed:Connect(function()
+        ToggleNames(names.Value)
+    end)
+    local esp = Instance.new("BoolValue", parent)
+    esp.Name = "ESP"
+    esp.Changed:Connect(function()
+        ToggleEsp(esp.Value)
+    end)
+    local chams = Instance.new("BoolValue", parent)
+    chams.Name = "Chams"
+    local aimbot = Instance.new("BoolValue", parent)
+    aimbot.Name = "Aimbot"
+    local ctotp = Instance.new("BoolValue", parent)
+    ctotp.Name = "CtoTeleport"
+    local fly = Instance.new("BoolValue", parent)
+    fly.Name = "Fly"
+end
+
 local function InitUI()
     if (not pGui) then return end
     local coreGui = Instance.new("ScreenGui")
+    coreGui.Name = "CCoreGui"
     coreGui.ResetOnSpawn = false
     coreGui.Parent = pGui
+
+    SetupBools(coreGui)
 
     local mainFrame = Instance.new("Frame")
 
@@ -174,13 +402,89 @@ local function InitUI()
     ccTFrame.Parent = list
     ccTFrame.TextBox:Destroy()
     ccTFrame.TextLabel.Text = "Click to Teleport"
-    CreateButton("CtoTeleport", ccTFrame)
+    CreateButton("CtoTeleport", ccTFrame, coreGui)
+
+    local espFrame = teleportFrame:Clone()
+    espFrame.Parent = list
+    espFrame.TextBox:Destroy()
+    espFrame.TextLabel.Text = "ESP"
+    CreateButton("ESP", espFrame, coreGui)
+
+    local chamsFrame = teleportFrame:Clone()
+    chamsFrame.Parent = list
+    chamsFrame.TextBox:Destroy()
+    chamsFrame.TextLabel.Text = "Chams"
+    CreateButton("Chams", chamsFrame, coreGui)
+
+    local namesFrame = teleportFrame:Clone()
+    namesFrame.Parent = list
+    namesFrame.TextBox:Destroy()
+    namesFrame.TextLabel.Text = "Bigger Names"
+    CreateButton("Names", namesFrame, coreGui)
+
+    local spinFrame = teleportFrame:Clone()
+    spinFrame.Parent = list
+    spinFrame.TextBox:Destroy()
+    spinFrame.TextLabel.Text = "Spinbot"
+    CreateButton("Spinbot", spinFrame, coreGui)
+
+    local speedFrame = teleportFrame:Clone()
+    speedFrame.Parent = list
+    speedFrame.TextBox.PlaceholderText = "Value..."
+    speedFrame.TextLabel.Text = "Speed"
+    speedFrame.TextBox.FocusLost:Connect(function(enter)
+        if (enter) then
+            ToggleSpeed(false, 0)
+            ToggleSpeed(true, speedFrame.TextBox.Text)
+            speedFrame.TextBox.Text = ""
+        end
+    end)
+    local speedClr = Instance.new("TextButton", speedFrame)
+    speedClr.Size = UDim2.fromScale(.104, .802)
+    speedClr.Position = UDim2.fromScale(.430,.1)
+    speedClr.Font = Enum.Font.ArimoBold
+    speedClr.BackgroundColor3 = Color3.fromRGB(61,61,61)
+    speedClr.TextScaled = true
+    speedClr.TextColor3 = Color3.fromRGB(255,0,0)
+    speedClr.Text = "Clr"
+    local speedClrStroke = Instance.new("UIStroke", speedClr)
+    speedClrStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+    speedClrStroke.Color = Color3.fromRGB(255,255,255)
+    local speedClrCorner = Instance.new("UICorner", speedClr)
+    speedClr.Activated:Connect(function()
+        ToggleSpeed(false, 0)
+    end)
+
+    local jumpFrame = speedFrame:Clone()
+    jumpFrame.Parent = list
+    jumpFrame.TextLabel.Text = "Jump"
+    jumpFrame.TextBox.FocusLost:Connect(function(enter)
+        if (enter) then
+            ToggleJump(false, 0)
+            ToggleJump(true, jumpFrame.TextBox.Text)
+            jumpFrame.TextBox.Text = ""
+        end
+    end)
+    jumpFrame.TextButton.Activated:Connect(function()
+        ToggleJump(false, 0)
+    end)
+
+    local glueFrame = speedFrame:Clone()
+    glueFrame.Parent = list
+    glueFrame.TextLabel.Text = "Glue"
+    glueFrame.TextBox.PlaceholderText = "Username..."
+    glueFrame.TextBox.FocusLost:Connect(function(enter)
+        if (enter) then
+            ToggleGlueHack(true, glueFrame.TextBox.Text)
+            glueFrame.TextBox.Text = ""
+        end
+    end)
+    glueFrame.TextButton.Activated:Connect(function()
+        ToggleGlueHack(false, "")
+    end)
+
     
     mainFrame.Parent = coreGui
 end
 
 InitUI()
-
-local functions = {
-    
-}
